a51b4fe93b2dac713dd5b737143e170e6b96fa8d
Wahid-debug <wahid.zamal@gmail.com>
2021-08-25 10:55:01 +0530

    User Pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 //import './App.css';
+import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom';
 import Todo from './component/todo/todo';
 import User from './component/user/User';
+
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      <Router>
+          <ul>
+            <li><NavLink to='/'>Todo</NavLink></li>
+            <li><NavLink to='/users'>UserPosts</NavLink></li>
+          </ul>
+        <Switch>
+          <Route exact path='/' component={Todo} />
+          <Route exact path='/users' component={User} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/component/user/User.js
+++ src/component/user/User.js
@@ -1,19 +1,60 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import {
+  Box,
+  CssBaseline,
+  Typography,
+  Card,
+  CardContent,
+  Grid,
+  Container,
+} from "@material-ui/core";
+import Pagination from "@material-ui/lab/Pagination";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const User = () => {
+function User() {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const loadPosts = async () => {
+      console.log("page", page)
+    const res = await axios.get(`https://reqres.in/api/users?page=${page}`);
+    setPosts(res.data.data);
+    console.log(res.data.data)
+  };
+  useEffect(() => {
+    loadPosts();
+  }, [page]);
 
-    useEffect(() => {
-        const data = axios.get(`https://reqres.in/api/users`).then((response) => {
-            console.log(data);
-        });
-
-    }, [])
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
+  return (
+    <div className="App">
+      <CssBaseline />
+      <Container component={Box} py={3}>
+        <Grid container spacing={2}>
+          {posts.map((post) => (
+            <Grid item sm={3}>
+              <Card key={post.id} style={{ height: 250 }}>
+                <CardContent>
+                  <Typography variant="h6">
+                    <img style={{height:"50%",borderRadius:50}} src={post.avatar} />
+                  </Typography>
+                  <Typography variant="body1">{`${post.first_name} ${post.last_name} `}</Typography>
+                </CardContent>
+                <Typography varient="body2">{`Email- ${post.email}`}</Typography>
+                <Typography varient="body2">{`ID- ${post.id}`}</Typography>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Box py={3} display="flex" justifyContent="center">
+          <Pagination
+            count={2}
+            color="secondary"
+            variant="outlined"
+            onChange={(e, value) => setPage(value)}
+          />
+        </Box>
+      </Container>
+    </div>
+  );
 }
 
 export default User;
